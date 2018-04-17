25447cccded587ac111ded8299052aa66154af3f
Andy <andyfuzstar@hotmail.com>
2018-04-17 19:46:49 +0100

    Rename of folder

---
+++ c-sharp/CodifyTestHarness/2. Arrays/OddOccurances.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace CodalityTestLibrary
+{
+    /// <summary>
+    /// Write a function that, given an array A consisting of N integers 
+    /// The array contains an odd number of elements, and each element of the 
+    /// array can be paired with another element that has the same value, 
+    /// except for one element that is left unpaired. 
+    /// Returns the value of the unpaired element.
+    /// </summary>
+    public class OddOccurances
+    {
+        public int solution(int[] numericArray)
+        {
+            // Handle empty array
+            if(numericArray.Length == 0)
+            {
+                return 0;
+            }
+
+            // Group the array, get any orphan values, grab the first
+            // assuming the array is correctly constructed
+            return numericArray.GroupBy(x => x)
+                               .Where(y => y.Count()%2 == 1)
+                               .Select(g => g.Key)
+                               .FirstOrDefault();
+                  
+        } 
+    }
+}
